8f7fd0e247a4af7ffb2ed40dd26f8ccf43e292a3
wuzhang <930116897@qq.com>
2017-06-15 13:50:26 +0800

    Add file add test scene and scripts!

---
+++ Assets/Scripts/HelloGitHub.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloGitHub : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hello GitHub!");
+    }
+
+}
